c9f548cf0dea0e0feceed36c9b95c6714eda0cc3
sungmin cha <csm9493@gmail.com>
2020-11-23 17:20:46 +0900

    update .sh

--- train_fbi_emse.sh
+++ train_fbi_emse.sh
@@ -1,77 +1,90 @@
-
+#! /bin/bash
 # train PGE Net for a synthetic Poisson-Gaussian noise on a specific noise (alpha = 0.01, beta = 0.02)
 CUDA_VISIBLE_DEVICES=3 python main.py \
 --date 201104 \
 --seed 0 \
 --noise-type 'Poisson-Gaussian' \
---loss-function 'Noise_est' \
---model-type 'PGE_Net' \
+--loss-function 'EMSE_Affine' \
+--model-type 'FBI_Net' \
 --data-type 'RawRGB' \ 
 --data-name 'fivek' \ 
 --alpha 0.01 \
 --beta 0.02 \
 --batch-size 1 \
 --lr 0.0001 \
---crop-size 200
-
+--num-layers 17 \
+--num-filters 64 \
+--crop-size 220 \
+--pge-weight-dir  '201104_Noise_est_RawRGB_fivek_alpha_0.01_beta_0.02_PGE_Net_cropsize_200.w' 
 
 # # train PGE Net for a synthetic Poisson-Gaussian noise on a random noise
 # CUDA_VISIBLE_DEVICES=3 python main.py \
 # --date 201104 \
 # --seed 0 \
 # --noise-type 'Poisson-Gaussian' \
-# --loss-function 'Noise_est' \
-# --model-type 'PGE_Net' \
+# --loss-function 'EMSE_Affine' \
+# --model-type 'FBI_Net' \
 # --data-type 'RawRGB' \ 
 # --data-name 'fivek' \ 
 # --alpha 0.0 \
 # --beta 0.0 \
 # --batch-size 1 \
 # --lr 0.0001 \
-# --crop-size 200
-
+# --num-layers 17 \
+# --num-filters 64 \
+# --crop-size 220 \
+# --pge-weight-dir 'dir_for_pge_net' 
 
 # # train PGE Net for FMD dataset
 # CUDA_VISIBLE_DEVICES=3 python main.py \
 # --date 201104 \
 # --seed 0 \
 # --noise-type 'Poisson-Gaussian' \
-# --loss-function 'Noise_est' \
-# --model-type 'PGE_Net' \
+# --loss-function 'EMSE_Affine' \
+# --model-type 'FBI_Net' \
 # --data-type 'Grayscale' \ 
 # --data-name 'CF_FISH' \ # 'CF_FISH', 'CF_MICE', 'TP_MICE' 
 # --alpha 0.0 \
 # --beta 0.0 \
 # --batch-size 1 \
 # --lr 0.0001 \
-# --crop-size 200
+# --num-layers 17 \
+# --num-filters 64 \
+# --crop-size 220 \
+# --pge-weight-dir 'dir_for_pge_net' 
 
 # # train PGE Net for SIDD dataset
 # CUDA_VISIBLE_DEVICES=3 python main.py \
 # --date 201104 \
 # --seed 0 \
 # --noise-type 'Poisson-Gaussian' \
-# --loss-function 'Noise_est' \
-# --model-type 'PGE_Net' \
+# --loss-function 'EMSE_Affine' \
+# --model-type 'FBI_Net' \
 # --data-type 'RawRGB' \ 
-# --data-name 'SIDD' \ # 'CF_FISH', 'CF_MICE', 'TP_MICE' 
+# --data-name 'SIDD' \ 
 # --alpha 0.0 \
 # --beta 0.0 \
 # --batch-size 1 \
 # --lr 0.0001 \
-# --crop-size 200
+# --num-layers 17 \
+# --num-filters 64 \
+# --crop-size 220 \
+# --pge-weight-dir 'dir_for_pge_net' 
 
 # # train PGE Net for DND dataset
 # CUDA_VISIBLE_DEVICES=3 python main.py \
 # --date 201104 \
 # --seed 0 \
 # --noise-type 'Poisson-Gaussian' \
-# --loss-function 'Noise_est' \
-# --model-type 'PGE_Net' \
+# --loss-function 'EMSE_Affine' \
+# --model-type 'FBI_Net' \
 # --data-type 'RawRGB' \ 
-# --data-name 'DND' \ # 'CF_FISH', 'CF_MICE', 'TP_MICE' 
+# --data-name 'DND' \ 
 # --alpha 0.0 \
 # --beta 0.0 \
 # --batch-size 1 \
 # --lr 0.0001 \
-# --crop-size 200
+# --num-layers 17 \
+# --num-filters 64 \
+# --crop-size 220 \
+# --pge-weight-dir 'dir_for_pge_net' 
--- train_fbi_mse.sh
+++ train_fbi_mse.sh
@@ -1,77 +1,85 @@
-
+#! /bin/bash
 # train PGE Net for a synthetic Poisson-Gaussian noise on a specific noise (alpha = 0.01, beta = 0.02)
 CUDA_VISIBLE_DEVICES=3 python main.py \
 --date 201104 \
 --seed 0 \
 --noise-type 'Poisson-Gaussian' \
---loss-function 'Noise_est' \
---model-type 'PGE_Net' \
+--loss-function 'MSE_Affine' \
+--model-type 'FBI_Net' \
 --data-type 'RawRGB' \ 
 --data-name 'fivek' \ 
 --alpha 0.01 \
 --beta 0.02 \
 --batch-size 1 \
 --lr 0.0001 \
---crop-size 200
-
+--num-layers 17 \
+--num-filters 64 \
+--crop-size 220 \
 
 # # train PGE Net for a synthetic Poisson-Gaussian noise on a random noise
 # CUDA_VISIBLE_DEVICES=3 python main.py \
 # --date 201104 \
 # --seed 0 \
 # --noise-type 'Poisson-Gaussian' \
-# --loss-function 'Noise_est' \
-# --model-type 'PGE_Net' \
+# --loss-function 'MSE_Affine' \
+# --model-type 'FBI_Net' \
 # --data-type 'RawRGB' \ 
 # --data-name 'fivek' \ 
 # --alpha 0.0 \
 # --beta 0.0 \
 # --batch-size 1 \
 # --lr 0.0001 \
-# --crop-size 200
-
+# --num-layers 17 \
+# --num-filters 64 \
+# --crop-size 220 
 
 # # train PGE Net for FMD dataset
 # CUDA_VISIBLE_DEVICES=3 python main.py \
 # --date 201104 \
 # --seed 0 \
 # --noise-type 'Poisson-Gaussian' \
-# --loss-function 'Noise_est' \
-# --model-type 'PGE_Net' \
+# --loss-function 'MSE_Affine' \
+# --model-type 'FBI_Net' \
 # --data-type 'Grayscale' \ 
 # --data-name 'CF_FISH' \ # 'CF_FISH', 'CF_MICE', 'TP_MICE' 
 # --alpha 0.0 \
 # --beta 0.0 \
 # --batch-size 1 \
 # --lr 0.0001 \
-# --crop-size 200
+# --num-layers 17 \
+# --num-filters 64 \
+# --crop-size 220 
 
 # # train PGE Net for SIDD dataset
 # CUDA_VISIBLE_DEVICES=3 python main.py \
 # --date 201104 \
 # --seed 0 \
 # --noise-type 'Poisson-Gaussian' \
-# --loss-function 'Noise_est' \
-# --model-type 'PGE_Net' \
+# --loss-function 'MSE_Affine' \
+# --model-type 'FBI_Net' \
 # --data-type 'RawRGB' \ 
-# --data-name 'SIDD' \ # 'CF_FISH', 'CF_MICE', 'TP_MICE' 
+# --data-name 'SIDD' \ 
 # --alpha 0.0 \
 # --beta 0.0 \
 # --batch-size 1 \
 # --lr 0.0001 \
-# --crop-size 200
+# --num-layers 17 \
+# --num-filters 64 \
+# --crop-size 220 
 
 # # train PGE Net for DND dataset
 # CUDA_VISIBLE_DEVICES=3 python main.py \
 # --date 201104 \
 # --seed 0 \
 # --noise-type 'Poisson-Gaussian' \
-# --loss-function 'Noise_est' \
-# --model-type 'PGE_Net' \
+# --loss-function 'MSE_Affine' \
+# --model-type 'FBI_Net' \
 # --data-type 'RawRGB' \ 
-# --data-name 'DND' \ # 'CF_FISH', 'CF_MICE', 'TP_MICE' 
+# --data-name 'DND' \ 
 # --alpha 0.0 \
 # --beta 0.0 \
 # --batch-size 1 \
 # --lr 0.0001 \
-# --crop-size 200
+# --num-layers 17 \
+# --num-filters 64 \
+# --crop-size 220 
